36794897546741fb8e7f0ee885cc743928dee9e9
Dmitrij Kolotusha <suvstreet@gmail.com>
2024-05-11 23:26:55 +0300

    Описание данных

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPhone } from '@fortawesome/free-solid-svg-icons'
-import { faSun } from '@fortawesome/free-regular-svg-icons'
-import { faReact } from '@fortawesome/free-brands-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faPhone } from '@fortawesome/free-solid-svg-icons'
+// import { faSun } from '@fortawesome/free-regular-svg-icons'
+// import { faReact } from '@fortawesome/free-brands-svg-icons'
 import s from 'styled-components'
 
 const Div = s.div`
@@ -15,9 +15,6 @@ function App() {
 	return (
 		<>
 			<Div>App</Div>
-			<FontAwesomeIcon icon={faPhone} size='10x' color='blue' />
-			<FontAwesomeIcon icon={faSun} size='10x' style={{ color: "#ffd43b" }} />
-			<FontAwesomeIcon icon={faReact} size='10x' />
 		</>
 	)
 }
